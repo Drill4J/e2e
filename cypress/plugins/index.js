b912220a20ad9dd2c64a11d864ac39f7a5fe224b
Ilia_Solovev1 <49729985+IliaSolovev@users.noreply.github.com>
2021-10-27 09:36:21 +0300

    refactor: start app scripts (#2)

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -49,40 +49,22 @@ module.exports = (on) => {
       return null;
     },
     async startAdmin() {
-      const containersIds = await promisifiedExec("docker-compose -f ./docker/docker-compose.admin.yml up -d");
+      const containersIds = await dockerComposeUp("./docker/docker-compose.admin.yml", "./docker/docker-compose.admin.env");
       console.log(`Started containers: ${containersIds}`);
       return null;
     },
-    async startPetclinic({ agentId = "dev-pet-standalone", build = "0.1.0" }) {
-      const data = `
-      PET_STANDALONE_BUILD=${build}
-      agentId=${agentId}
-      PET_STANDALONE_PORT=8087
-      adminAddress=drill-admin:8090
-      LOG_LEVEL=DEBUG
-      `;
-      writeFile("./docker/docker-compose-pet.env", data, "utf-8", (err) => {
-        if (err) {
-          console.log(err.message);
-        }
-      });
-      await promisifiedExec("docker-compose -f ./docker/docker-compose-pet.yml --env-file ./docker/docker-compose-pet.env up -d");
+    async startPetclinic({ build = "0.1.0" }) {
+      await dockerComposeUp(
+        "./docker/single-java-agent.yml",
+        `./docker/single-java-agent-build-${build}.env`,
+      );
       return null;
     },
-    async startPetclinicMicroservice({ groupId = "dev-pet-mcr", build = "0.1.0" }) {
-      const data = `
-      adminAddress=drill-admin:8090
-      PET_MCR_BUILD=${build}
-      AGENT_VERSION=latest
-      groupId=${groupId}
-      LOG_LEVEL=DEBUG
-      `;
-      writeFile("./docker/docker-compose-pet-mcr.env", data, "utf-8", (err) => {
-        if (err) {
-          console.log(err.message);
-        }
-      });
-      await promisifiedExec("docker-compose -f ./docker/docker-compose-pet-mcr.yml --env-file ./docker/docker-compose-pet-mcr.env up -d");
+    async startPetclinicMicroservice({ build = "0.1.0" }) {
+      await dockerComposeUp(
+        "./docker/microservice-java-agents.yml",
+        `./docker/microservice-java-agents-build-${build}.env`,
+      );
       return null;
     },
     async startPetclinicMultinstaces({ agentId = "pet-multinstances", build = "0.1.0" }) {
@@ -98,33 +80,28 @@ module.exports = (on) => {
       PET_NODE_2_PORT=8086
       BALANCER_PORT=8087
       `;
-      writeFile("./docker/docker-compose-pet-multinstances.env", data, "utf-8", (err) => {
+      writeFile("./docker/multinstances-single-java-agent.env", data, "utf-8", (err) => {
         if (err) {
           console.log(err.message);
         }
       });
       await promisifiedExec(
-        "docker-compose -f ./docker/docker-compose-pet-multinstances.yml --env-file ./docker/docker-compose-pet-multinstances.env up -d",
+        "docker-compose -f ./docker/multinstances-single-java-agent.yml --env-file ./docker/multinstances-single-java-agent.env up -d",
       );
       return null;
     },
-    async startPetclinicAutoTests({ agentId = "dev-pet-standalone", petclinicPort = "8087" }) {
-      const data = `
-        agentId=${agentId}
-        petclinicPort=${petclinicPort}
-      `;
-      writeFile("./docker/start-petclinic-tests.env", data, "utf-8", (err) => {
-        if (err) {
-          console.log(err.message);
-        }
-      });
-      await promisifiedExec("docker-compose -f ./docker/start-petclinic-tests.yml --env-file ./docker/start-petclinic-tests.env up");
+    async startPetclinicAutoTests() {
+      await promisifiedExec("docker-compose -f ./docker/single-java-agents-tests.yml --env-file ./docker/single-java-agents-tests.env up");
+      console.log("petclinic tests container exited");
+      return null;
+    },
+    async startPetclinicMultinstacesAutoTests() {
+      await promisifiedExec("docker-compose -f ./docker/single-java-agents-tests.yml --env-file ./docker/single-java-agents-tests.env up");
       console.log("petclinic tests container exited");
       return null;
     },
     async startPetclinicMicroserviceAutoTests() {
-      await promisifiedExec("docker-compose -f ./docker/start-petclinic-microservice-tests.yml up");
-      console.log("petclinic microservice tests container exited");
+      await promisifiedExec("docker-compose -f ./docker/microservice-java-agents-tests.yml up");
       return null;
     },
   });
@@ -140,4 +117,8 @@ function promisifiedExec(command) {
       resolve(out);
     });
   });
+}
+
+async function dockerComposeUp(composePath, envFilePath) {
+  return promisifiedExec(`docker-compose -f ${composePath} --env-file ${envFilePath} up -d`);
 }
