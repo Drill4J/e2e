3c8defa8b40d762346b7ed8fa000cd6ade275a13
ilia_solovev1 <ilia_solovev1@epam.com>
2021-12-23 20:26:29 +0300

    feat: add in description information about published version

--- .github/actions/start-tests.js
+++ .github/actions/start-tests.js
@@ -25,6 +25,7 @@ try {
     const setupsConfig = JSON.parse(fs.readFileSync("./setups.json", "utf8"));
     const [publishedArtifactId, publishedVersion] = Object.entries(github.context.payload.client_payload)[0];
     console.log(`Published artifact: ${publishedArtifactId} - ${publishedVersion}`);
+    core.setOutput('description', `${publishedArtifactId}: ${publishedVersion}`)
 
     axios.get("https://raw.githubusercontent.com/Drill4J/vee-ledger/main/ledger.json").then(async ({data: ledgerData}) => {
         const {setups} = ledgerData;
